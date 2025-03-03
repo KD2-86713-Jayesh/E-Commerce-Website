d744ad6a810693becbb68090076c53081014487e
Jayesh139 <jkwagh2017@gmail.com>
2025-03-04 01:30:02 +0530

    Navbar Setup

--- FrontEnd/src/Components/Common/Header.jsx
+++ FrontEnd/src/Components/Common/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Topbar from '../Layout/Topbar'
+import Navbar from './Navbar'
 
 export const Header = () => {
   return (
-    <div>
+    <header>
         {/*TOP BAR */}
         <Topbar></Topbar>
         {/* NAV BAR */}
+        <Navbar></Navbar>
         {/*CART DRAWER */}
-    </div>
+    </header>
   )
 }
